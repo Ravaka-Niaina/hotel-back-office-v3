5c7c0d2edd18969c7dbed0af495f6165e47e3931
f <cedrictianarabarijohn@gmail.com>
2022-08-17 17:29:39 +0400

    Changing buttons to customizedButton

--- src/components/accessRight/AddAccessRightDialog.js
+++ src/components/accessRight/AddAccessRightDialog.js
@@ -93,23 +93,10 @@ const AddAccessRightDialog = (props) => {
 
   return (
     <>
-      <Button
+      <CustomizedButton
         onClick={handleClickOpen}
-        variant="contained"
-        component={RouterLink}
-        to="#"
-        startIcon={<Iconify icon="eva:plus-fill" />}
-        sx={{
-          background: `linear-gradient(270deg, #33647E 0%, #59A2BE 100%);`,
-          boxShadow:
-            ' -4px -4px 15px  #FFFFFF, 4px 4px 20px rgba(111, 140, 176, 0.41),2px 2px 4px rgba(114, 142, 171, 0.1)',
-          fontSize: 12,
-          px: 5,
-          py: 1,
-        }}
-      >
-        Ajouter
-      </Button>
+        text={`Ajouter`}
+      />
       <Dialog open={open} onClose={handleClose} maxWidth={'sm'}>
         <CustomizedDialogTitle text="Ajouter un nouveau droit d'accès" />
         <DialogContent style={{ backgroundColor: '#E8F0F8', paddingTop: 15 }}>
--- src/components/roomType/AddRoomTypeDialog.js
+++ src/components/roomType/AddRoomTypeDialog.js
@@ -61,22 +61,7 @@ const AddRoomTypeDialog = () => {
 
   return (
     <>
-      <Button
-        onClick={handleClickOpen}
-        variant="contained"
-        component={RouterLink}
-        to="#"
-        startIcon={<Iconify icon="eva:plus-fill" />}
-        sx={{
-          background: `linear-gradient(270deg, #33647E 0%, #59A2BE 100%);`,
-          boxShadow:
-            ' -4px -4px 15px  #FFFFFF, 4px 4px 20px rgba(111, 140, 176, 0.41),2px 2px 4px rgba(114, 142, 171, 0.1)',
-          px: 5,
-          py: 1,
-        }}
-      >
-        Ajouter
-      </Button>
+      <CustomizedButton onClick={handleClickOpen} text={`Ajouter`} />
       <Dialog open={open} onClose={handleClose} maxWidth={'xl'}>
         <CustomizedDialogTitle text="Ajouter un nouveau type de chambre" />
 
