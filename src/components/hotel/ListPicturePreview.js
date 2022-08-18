ac131be3f82f6a0ec83a88e9b0ac254d5cbbdc6d
tHoxic01H <andriantsilavinaanthony@gmail.com>
2022-08-18 17:45:32 +0300

    fix list of picture

--- src/components/hotel/ListPicturePreview.js
+++ src/components/hotel/ListPicturePreview.js
@@ -5,14 +5,14 @@ const ListPicturePreview = (props) => {
     const {itemData} = props;
     console.log(itemData);
     return (
-        <ImageList sx={{ width: 500, height: 450 }} cols={2} rowHeight={164}>
+        <ImageList sx={{ width: 1, height: 450 }} cols={2} rowHeight={164}>
             {itemData.map((item) => (
-                <ImageListItem key={item}>
+                <ImageListItem key={item} sx={{border:'2px black solid'}}>
                     <img
-                        src={`${item}?w=164&h=164&fit=crop&auto=format`}
-                        srcSet={`${item}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                        src={`${item}`}
                         alt='imge'
                         loading="lazy"
+                        style={{ maxWidth: '300px', maxHeight: '200px', width: 'auto', height: 'auto', margin: '2px 2px', padding: '0 0' }}
                     />
                 </ImageListItem>
             ))}
