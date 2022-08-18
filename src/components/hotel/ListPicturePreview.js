80312d0bc25cb4cd9d187d31936df59d965866e8
tHoxic01H <andriantsilavinaanthony@gmail.com>
2022-08-18 17:56:38 +0300

    fix  picture list layout

--- src/components/hotel/ListPicturePreview.js
+++ src/components/hotel/ListPicturePreview.js
@@ -5,14 +5,14 @@ const ListPicturePreview = (props) => {
     const {itemData} = props;
     console.log(itemData);
     return (
-        <ImageList sx={{ width: 1, height: 450 }} cols={2} rowHeight={164}>
+        <ImageList sx={{ width: 1, height: 450 }}  rowHeight={164} gap={8} variant="masonry">
             {itemData.map((item) => (
-                <ImageListItem key={item} sx={{border:'2px black solid'}}>
+                <ImageListItem key={item}  style={{border:'1px white solid'}}>
                     <img
                         src={`${item}`}
                         alt='imge'
                         loading="lazy"
-                        style={{ maxWidth: '300px', maxHeight: '200px', width: 'auto', height: 'auto', margin: '2px 2px', padding: '0 0' }}
+                        
                     />
                 </ImageListItem>
             ))}
