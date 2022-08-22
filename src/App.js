9b63efaa56a14696fdeca6179be9c798c1773d95
Cedric <cedrictianarabarijohn@gmail.com>
2022-08-22 17:07:03 +0400

    Fix ModifyPoliticDialog

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import 'simplebar/src/simplebar.css';
 
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+
 // components
 import Wrapper from './components/context/Wrapper';
 import ThemeProvider from './theme';
 
+import ScrollToTop from './utils/ScrollToTop';
+import { BaseOptionChartStyle } from './components/chart/BaseOptionChartStyle';
+import Router from './config/routes/routes';
 // css
 import './assets/css/fonts.css'
-import Test from "./Test";
 
 const App = () => {
     const [lang, setLang] = useState("en")
@@ -19,7 +22,9 @@ const App = () => {
                 <HelmetProvider>
                     <BrowserRouter>
                         <ThemeProvider>
-                            <Test />
+                            <ScrollToTop />
+                            <BaseOptionChartStyle />
+                            <Router />
                         </ThemeProvider>
                     </BrowserRouter>
                 </HelmetProvider>
