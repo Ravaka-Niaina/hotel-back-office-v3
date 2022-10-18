1bee9391e751cf3026fb3deb051bd5e8a9ce0544
Tsitohaina Andriantsilavina <89443430+tHoxic01H@users.noreply.github.com>
2022-10-18 20:26:27 +0300

    feat:add pagination to canceling politic list

--- src/views/Politic.js
+++ src/views/Politic.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Table, Stack, TableRow, TableBody, Container, Typography, TableContainer } from '@mui/material';
+import { Table, Stack, TableRow, TableBody, Container, Typography, TableContainer,TablePagination,TableCell,Box } from '@mui/material';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
@@ -14,6 +14,7 @@ import { getPolitics } from '../services/Politic';
 import { ThemeContext } from '../components/context/Wrapper';
 import PoliticMoreMenu from '../components/politic/PoliticMoreMenu';
 import CustomizedPaperOutside from '../components/CustomizedComponents/CustomizedPaperOutside';
+import CustomizedLinearProgress from '../components/CustomizedComponents/CustomizedLinearProgress';
 import { lightBackgroundToTop } from '../components/CustomizedComponents/NeumorphismTheme';
 
 const TABLE_HEAD = [
@@ -25,12 +26,79 @@ const TABLE_HEAD = [
 
 const Politic = () => {
   const context = useContext(ThemeContext);
-  const [politicList, setPoliticList] = useState(new Array(0));
   const order = 'asc';
   const selected = [];
   const orderBy = 'name';
   const filterName = '';
-  const getAllPolitics = () => {
+
+  const [politicList, setPoliticList] = useState(new Array(0));
+
+  const [resultCount, setResultCount] = useState(0);
+
+  const [page, setPage] = useState(1);
+
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChangePage = (e, p, row = rowsPerPage) => {
+    fetchFilter(p + 1, row);
+  }
+  const handleChangeRowsPerPage = (e) => {
+    const row = parseInt(e.target.value, 10);
+    handleChangePage(null, 0, row);
+  };
+  const fetchFilter = (p = 1, row = rowsPerPage) => {
+    setLoading(true);
+    setPage(p);
+    setRowsPerPage(row);
+    const payload = {
+      tableName: 'politiqueAnnulation',
+      valuesToSearch: [],
+      fieldsToPrint: [],
+      nbContent: 200,
+      numPage: 1,
+    };
+    const idToken = localStorage.getItem('id_token');
+    getPolitics({ ...payload }, idToken)
+      .then((result) => {
+        try {
+          const status = result?.status;
+          if (status === 200) {
+            const list = result.data?.list;
+            setPoliticList(list);
+            setResultCount(result.data.nbResult);
+          }
+          else if (result.data.errors) {
+            const item = Object.keys(result.data.errors).filter((e, i) => i === 0)[0];
+            const indication = result.data.errors[item];
+            const message = `${item}: ${indication}`;
+            context.changeResultErrorMessage(message);
+            context.showResultError(true);
+          }
+          else if (result.data.message) {
+            context.changeResultErrorMessage(result.data.message);
+            context.showResultError(true);
+          }
+          else {
+            context.changeResultErrorMessage('Une erreur est survenue lors du chargement des données');
+            context.showResultError(true);
+            context.showResultError(true);
+          }
+        } catch (err) {
+          context.changeResultErrorMessage(err.message);
+          context.showResultError(true);
+        }
+      })
+      .catch((err) => {
+        context.changeResultErrorMessage(err.message);
+        context.showResultError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+  const getAllPolitics = (p = 1, row = rowsPerPage) => {
     context.showLoader(true);
     const payload = {
       tableName: 'politiqueAnnulation',
@@ -41,20 +109,33 @@ const Politic = () => {
     };
     const idToken = localStorage.getItem('id_token');
     getPolitics({ ...payload }, idToken)
-      .then((datas) => {
-        try {
-          const status = datas?.status;
-          console.log(datas)
+    .then((result) => {
+      try {
+          const status = result?.status;
           if (status === 200) {
-            const list = datas.data?.list;
+            const list = result.data?.list;
             setPoliticList(list);
-          } else {
-            context.changeResultErrorMessage('Un problème est survenu, veuillez rafraichir la page!');
+            setResultCount(result.data.nbResult);
+          }
+          else if (result.data.errors) {
+            const item = Object.keys(result.data.errors).filter((e, i) => i === 0)[0];
+            const indication = result.data.errors[item];
+            const message = `${item}: ${indication}`;
+            context.changeResultErrorMessage(message);
+            context.showResultError(true);
+          }
+          else if (result.data.message) {
+            context.changeResultErrorMessage(result.data.message);
+            context.showResultError(true);
+          }
+          else {
+            context.changeResultErrorMessage('Une erreur est survenue lors du chargement des données');
+            context.showResultError(true);
             context.showResultError(true);
           }
         } catch (err) {
-          context.changeResultErrorMessage(err.message);
-          context.showResultError(true);
+            context.changeResultErrorMessage(err.message);
+            context.showResultError(true);
         }
       })
       .catch((err) => {
@@ -104,6 +185,17 @@ const Politic = () => {
                   />
                 )}
                 <TableBody>
+                {
+                            loading && (
+                              <TableRow>
+                                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={TABLE_HEAD.length + 1}>
+                                  <Box sx={{ margin: 1, textAlign: 'center' }} >
+                                    <CustomizedLinearProgress />
+                                  </Box>
+                                </TableCell>
+                              </TableRow>
+                            )
+                  }
                   {politicList &&
                     politicList.map((row, index) => {
                       // const { _id, nom, isActif } = row;
@@ -135,9 +227,39 @@ const Politic = () => {
                         </TableRow>
                       );
                     })}
+                    {
+                          !loading && politicList.length < 1 && (
+                            <TableRow>
+                            <TableCell style={{ textAlign: 'center' }} colSpan={TABLE_HEAD.length + 1}>
+                                <CustomizedTitle text={`Pas de résultats`} color='#212B36' level={3} />
+                                <Typography variant="body2" align="center">
+                                  Pas de politiques d'annulations trouvés &nbsp;
+                                  {/* <strong>
+                                    &quot;
+
+                                    &quot;
+                                  </strong>. */}
+                                </Typography>
+                              </TableCell>
+                            </TableRow>
+                          )
+                    }
                 </TableBody>
               </Table>
             </TableContainer>
+            <TablePagination
+                  rowsPerPageOptions={[5, 10, 15]}
+                  component="div"
+                  count={resultCount}
+                  rowsPerPage={rowsPerPage}
+                  page={page - 1}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                  labelRowsPerPage='Lignes par page'
+                  labelDisplayedRows={({ from, to, count, page }) => {
+                    return `Page ${page + 1} :   ${from} - ${to} sur ${count}`
+                  }}
+            />
           </Scrollbar>
         </CustomizedPaperOutside>
       </Container>
