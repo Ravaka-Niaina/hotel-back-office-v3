b6322ddffe88bab97c317e12ae71109f8972f081
Ravaka-Niaina <103428442+Ravaka-Niaina@users.noreply.github.com>
2022-11-03 11:54:08 +0300

    fix: add www to url front office

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 // const hosturl = 'http://localhost:3000';
 const hosturl = 'http://aiolia.us:3000';
-const frontOffice = 'http://aiolia:5000';
+const frontOffice = 'http://www.aiolia.us:5000';
 
 const config = {
     host: hosturl,
