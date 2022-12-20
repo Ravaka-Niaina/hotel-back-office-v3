e1094716e6638e4aa07f6d407d95ab0eb5796f3f
Ravaka-Niaina <103428442+Ravaka-Niaina@users.noreply.github.com>
2022-12-20 08:37:38 +0300

    feat: historic modif room type (still static)

---
+++ src/views/historic/HistoricRoomType.js
@@ -0,0 +1,241 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { Box, Table, Stack, TableRow, TableBody, Container, Typography, TableCell, TableContainer, TablePagination } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+import AddRatePlanDialog from '../../components/ratePlan/AddRatePlanDialog';
+import ModifyRatePlanDialog from '../../components/ratePlan/ModifyRatePlanDialog';
+import RatePlanMoreMenu from '../../components/ratePlan/RatePlanMoreMenu';
+import CustomizedCheckbox from '../../components/CustomizedComponents/CustomizedCheckbox';
+import TableCellStyled from '../../components/CustomizedComponents/CustomizedTableCell';
+import Page from '../../components/Page';
+import Scrollbar from '../../components/Scrollbar';
+import { ThemeContext } from '../../components/context/Wrapper';
+import { UserListHead, UserListToolbar } from '../../components/table';
+import CustomizedTitle from '../../components/CustomizedComponents/CustomizedTitle';
+import CustomizedPaperOutside from '../../components/CustomizedComponents/CustomizedPaperOutside';
+import CustomizedButton from '../../components/CustomizedComponents/CustomizedButton';
+import CustomizedLinearProgress from '../../components/CustomizedComponents/CustomizedLinearProgress';
+import { lightBackgroundToTop } from '../../components/CustomizedComponents/NeumorphismTheme';
+import CustomizedIconButton from '../../components/CustomizedComponents/CustomizedIconButton';
+import Iconify from '../../components/Iconify';
+
+const TABLE_HEAD = [
+  { id: 'roomTypeName', label: 'nom type chambre', alignleft: true },
+  { id: 'modifier', label: 'modificateur', alignleft: true },
+  { id: 'modificationDate', label: 'date modification', alignleft: true },
+  { id: 'modifiedField', label: 'champ modifié', alignleft: true },
+  { id: 'oldValue', label: 'ancienne valeur', alignleft: true },
+  { id: 'newValue', label: 'nouvelle valeur', alignleft: true },
+];
+
+const mockupHistoricRoomTypeList = [
+  {
+    idRoomType: 'shkdh20u0shdlksjs',
+    roomTypeName: 'Chambre standard',
+    modifier: 'Ravaka Niaina Ratefiarivony',
+    modificationDate: '31-12-2022',
+    modifiedField: 'nom',
+    oldValue: 'Chambre Deluxe',
+    newValue: 'Chambre standard'
+  },
+  {
+    idRoomType: 'shkdh20u0shdlksjs',
+    roomTypeName: 'Chambre standard',
+    modifier: 'Ravaka Niaina Ratefiarivony',
+    modificationDate: '31-12-2022',
+    modifiedField: 'nom',
+    oldValue: 'Chambre Deluxe',
+    newValue: 'Chambre standard'
+  },
+  {
+    idRoomType: 'shkdh20u0shdlksjs',
+    roomTypeName: 'Chambre standard',
+    modifier: 'Ravaka Niaina Ratefiarivony',
+    modificationDate: '31-12-2022',
+    modifiedField: 'nom',
+    oldValue: 'Chambre Deluxe',
+    newValue: 'Chambre standard'
+  },
+  {
+    idRoomType: 'shkdh20u0shdlksjs',
+    roomTypeName: 'Chambre standard',
+    modifier: 'Ravaka Niaina Ratefiarivony',
+    modificationDate: '31-12-2022',
+    modifiedField: 'nom',
+    oldValue: 'Chambre Deluxe',
+    newValue: 'Chambre standard'
+  },
+  {
+    idRoomType: 'shkdh20u0shdlksjs',
+    roomTypeName: 'Chambre standard',
+    modifier: 'Ravaka Niaina Ratefiarivony',
+    modificationDate: '31-12-2022',
+    modifiedField: 'nom',
+    oldValue: 'Chambre Deluxe',
+    newValue: 'Chambre standard'
+  },
+];
+
+const HistoricRoomType = () => {
+  const context = useContext(ThemeContext);
+  const order = 'asc';
+  const selected = [];
+  const orderBy = 'name';
+  const [filterName, setFilterName] = useState('');
+
+  const [location , setLocation] = useState('list');
+
+  const [currentId , setCurrentId] = useState(-1);
+
+  const [historicRoomTypeList, setHistoricRoomTypeList] = useState(new Array(0));
+
+  const [resultCount, setResultCount] = useState(0);
+
+  const [page, setPage] = useState(1);
+
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChangePage = (e, p, row = rowsPerPage) => {
+  }
+  const handleChangeRowsPerPage = (e) => {
+    const row = parseInt(e.target.value, 10);
+    handleChangePage(null, 0, row);
+  };
+
+  const handleFilterByName = (event) => {
+    setFilterName(event.target.value);
+  };
+
+  const navigate = (itinerary , rateCurrentId = -1) => {
+    setCurrentId(rateCurrentId);
+    if(itinerary === 'addForm')
+    {
+      setLocation('addForm');
+    }
+    else if(itinerary === 'modifyForm')
+    {
+      setLocation('modifyForm');
+    }
+    else if(itinerary === 'list')
+    {
+      setLocation('list');
+    }
+  };
+  
+  useEffect(() => {
+    setHistoricRoomTypeList(mockupHistoricRoomTypeList);
+  }, []);
+
+  return (
+    <Page title="AIOLIA | Plans tarifaires">
+      <Container>
+          {
+            location === 'list' && (
+              <>
+                <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+                  <CustomizedTitle text="Plan tarifaire" size={20} />
+                  <CustomizedButton onClick={() => navigate('addForm')} text='Ajouter' component={RouterLink} to="#" />
+                </Stack>
+                <CustomizedPaperOutside sx={{ ...lightBackgroundToTop, background: '#E3EDF7', p: 5, minHeight: '100vh' }}>
+                  <UserListToolbar 
+                    numSelected={selected.length} 
+                    onFilterName={handleFilterByName}
+                  />
+                  {historicRoomTypeList && <Scrollbar>
+                    <TableContainer sx={{ minWidth: 800 }}>
+                      <Table>
+                        <UserListHead
+                          order={order}
+                          orderBy={orderBy}
+                          headLabel={TABLE_HEAD}
+                          rowCount={historicRoomTypeList.length}
+                          numSelected={selected.length}
+                        />
+                        <TableBody>
+                          {
+                            loading && (
+                              <TableRow>
+                                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={TABLE_HEAD.length + 1}>
+                                  <Box sx={{ margin: 1, textAlign: 'center' }} >
+                                    <CustomizedLinearProgress />
+                                  </Box>
+                                </TableCell>
+                              </TableRow>
+                            )
+                          }
+                          {historicRoomTypeList.map((row) => {
+                            const { 
+                              idRoomType, 
+                              roomTypeName, 
+                              modifier, 
+                              modificationDate, 
+                              modifiedField, 
+                              oldValue, 
+                              newValue 
+                            } = row;
+
+                            return (
+                              <TableRow
+                                hover
+                                key={idRoomType}
+                                tabIndex={-1}
+                                role="checkbox"
+                              >
+                                <TableCellStyled padding="checkbox">
+                                  <CustomizedCheckbox />
+                                </TableCellStyled>
+                                <TableCellStyled align="left">{roomTypeName}</TableCellStyled>
+                                <TableCellStyled align="left">{modifier}</TableCellStyled>
+                                <TableCellStyled align="left">{modificationDate}</TableCellStyled>
+                                <TableCellStyled align="left">{modifiedField}</TableCellStyled>
+                                <TableCellStyled align="left">{oldValue}</TableCellStyled>
+                                <TableCellStyled align="left">{newValue}</TableCellStyled>
+                              </TableRow>
+                            );
+                          })}
+                        {
+                          !loading && historicRoomTypeList.length < 1 && (
+                            <TableRow>
+                              <TableCell style={{ textAlign: 'center' }} colSpan={TABLE_HEAD.length + 1}>
+                                <CustomizedTitle text={`Pas de résultats`} color='#212B36' level={3} />
+                                <Typography variant="body2" align="center">
+                                  Aucun historique trouvé &nbsp;
+                                  {/* <strong>
+                                    &quot;
+                                    Statut: &nbsp; {filter.status === 'none' ? ' tous ,' : `${filter.status} ,`}
+                                    {
+                                      filter.dateOf !== 'none' && `  ${filter.dateOf} entre le ${format(new Date(filter.dateFrom), 'dd MMMM yyyy')} et ${format(new Date(filter.dateUntil), 'dd MMMM yyyy')}.`
+                                    }
+                                    &quot;
+                                  </strong>. */}
+                                </Typography>
+                              </TableCell>
+                            </TableRow>
+                          )
+                        }
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                    <TablePagination
+                      rowsPerPageOptions={[5, 10, 15]}
+                      component="div"
+                      count={resultCount}
+                      rowsPerPage={rowsPerPage}
+                      page={page - 1}
+                      onPageChange={handleChangePage}
+                      onRowsPerPageChange={handleChangeRowsPerPage}
+                      labelRowsPerPage='Lignes par page'
+                      labelDisplayedRows={({ from, to, count, page }) => `Page ${page + 1} :   ${from} - ${to} sur ${count}`}
+                    />
+                  </Scrollbar>}
+                </CustomizedPaperOutside>
+              </>
+            )
+          }     
+      </Container>
+    </Page>
+  );
+};
+
+export default HistoricRoomType;
