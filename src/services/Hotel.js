5e06af14d8fb55d306995b9278a7ce61d9728845
Ravaka-Niaina <103428442+Ravaka-Niaina@users.noreply.github.com>
2023-01-27 13:23:20 +0300

    debug: remove some unnecessary headers to query /hotel

--- src/services/Hotel.js
+++ src/services/Hotel.js
@@ -6,8 +6,6 @@ export const getHotelList = (payload) =>
         timeout: 10000,
         headers: {
             Authorization: localStorage.getItem("id_token"),
-            partner_id: localStorage.getItem("partner_id"),
-            hotel_id: 'temp',
         },
 });
 
