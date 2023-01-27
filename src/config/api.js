7f2b4f125e350057de49ee584bfdae57ebdcd157
Ravaka-Niaina <103428442+Ravaka-Niaina@users.noreply.github.com>
2023-01-27 14:40:34 +0300

    fix: correct frontOffice url

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 // const hosturl = 'http://localhost:3000';
 const hosturl = 'https://api.aiolia.us';
-const frontOffice = 'https://.aiolia.us';
+const frontOffice = 'https://aiolia.us';
 
 const config = {
     host: hosturl,
